4b53b7e0fcd3d89c30dc4481dce87ec8f6c0e97f
Edwin Chi Pineiro <chipineiro@gmail.com>
2019-02-17 00:34:03 -0600

    nivel agregado mensaje del nivel agregado

--- src/Eevee.java
+++ src/Eevee.java
@@ -2,13 +2,15 @@ public class Eevee {
     public static void main (String [] args)
     {
         String nombre = "Eevee";
-        String tipo = "Fuego";
-        String piedra = "Hada";
+        String tipo = "Normal";
+        String piedra = "Fuego";
+        int nivel = 10;
 
         if (piedra == "Fuego")
         {
             nombre= "Flareon";
             tipo= "Fuego";
+            nivel = nivel+1;
 
         }
 
@@ -19,19 +21,19 @@ public class Eevee {
 
         }
 
-         if (piedra == "Trueno")
-         {
+        if (piedra == "Trueno")
+        {
             nombre= "Jolteon";
             tipo= "Eléctrico";
 
-         }
+        }
 
         if (piedra == "Alba")
-            {
-                nombre= "Espeon";
-                tipo= "Psíquico";
+        {
+            nombre= "Espeon";
+            tipo= "Psíquico";
 
-            }
+        }
 
         if (piedra == "Hoja")
         {
@@ -64,7 +66,7 @@ public class Eevee {
 
         System.out.println("Nombre del pokemon: " + nombre);
         System.out.println("El tipo del pokemon es: " + tipo);
+        System.out.println("El nivel es: " + nivel);
         System.out.println("El tipo de piedra que sostiene es: " + piedra);
     }
 }
-
